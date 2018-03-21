4d03c6a0615a591a26556c2f87267a2c722c7960
Kyle <kyle.folkerth@gmail.com>
2018-03-20 19:15:39 -0700

    adds ids to HTML elements and starts site logic

---
+++ assets/javascript/marvel-logic.js
@@ -0,0 +1,19 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBdM92H4MBJ5N1SvTqMBbleOW46qLBPKcU",
+    authDomain: "mp-rps-8fccc.firebaseapp.com",
+    databaseURL: "https://mp-rps-8fccc.firebaseio.com",
+    projectId: "mp-rps-8fccc",
+    storageBucket: "mp-rps-8fccc.appspot.com",
+    messagingSenderId: "680192097997"
+  };
+  firebase.initializeApp(config);
+
+  $("#search-button").on("click", function(event) {
+    
+    event.preventDefault();
+    var searchType = $("#search-by").val();
+    console.log(searchType);
+
+  });
